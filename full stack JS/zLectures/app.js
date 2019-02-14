3ab88b05884474a8dda0c383f28a102cf058ed4c
Jessie Jones <jessiejonesofficial@gmail.com>
2019-02-14 11:22:51 -0700

    Updates from this moring.  Todo list is fully functional.

--- full stack JS/zLectures/app.js
+++ full stack JS/zLectures/app.js
@@ -1,21 +1,21 @@
-sessionStorage.setIem('name', 'John');
-var name = sessionStorage.getItem('name'
-document.getElementById()
+// sessionStorage.setIem('name', 'John');
+// var name = sessionStorage.getItem('name'
+// document.getElementById()
 
 
-document.storage.addEventListener("submit", function(e){
-    e.preventDefault()
-    var person = addPerson()
-    document.getElementById('normal').innerHTML = `<h1>Normal: ${person.fName} ${person.lName}</h1>`
- })
+// document.storage.addEventListener("submit", function(e){
+//     e.preventDefault()
+//     var person = addPerson()
+//     document.getElementById('normal').innerHTML = `<h1>Normal: ${person.fName} ${person.lName}</h1>`
+//  })
  
- function addPerson (){
-    var form = document.storage;
-    var newPerson = {
-        fName: form.fname.value,
-        lName: form.lname.value
-    }
-    form.fname.value = '';
-    form.lname.value = '';
-    return newPerson
- }
+//  function addPerson (){
+//     var form = document.storage;
+//     var newPerson = {
+//         fName: form.fname.value,
+//         lName: form.lname.value
+//     }
+//     form.fname.value = '';
+//     form.lname.value = '';
+//     return newPerson
+//  }
--- projects/axios-todo/app.js
+++ projects/axios-todo/app.js
@@ -1,78 +1,99 @@
-let newObject = {
-    title: "Buy kitten",
-    description: "Buy the kitten from the kitten factory on state street",
-    completed: false,
-    imgUrl: "http://placekitten.com/300/350"
- }
- let updatedObject = {
-    title: "This is an updated todo",
-    description: "Oh wooow!"
- }
+// let newObject = {
+//     title: "Buy kitten",
+//     description: "Buy the kitten from the kitten factory on state street",
+//     completed: false,
+//     imgUrl: "http://placekitten.com/300/350"
+//  }
+//  let updatedObject = {
+//     title: "This is an updated todo",
+//     description: "Oh wooow!"
+//  }
 
-//  axios.get("https://api.vschool.io/jessie/todo").then((response)=>{
-//     console.log(response.data)
-//     response.data.map(function(item){
-//         document.getElementById("newlist").innerHTML += `${item.title}<br>`
-//         document.getElementById("newlist").innerHTML += `${item.description}<br>`
-//         document.getElementById("newlist").innerHTML += `<img src="${item.imgUrl}"/<br><br>`
-// })}).catch(function(error){
-//     console.log(error)
-// })
+// dataArray = [];
 
 axios.get("https://api.vschool.io/jessie/todo").then(function(response){
-    var arr = response.data
+    totallyDisplayDataDudeBro(response.data)
+});
+
+function totallyDisplayDataDudeBro(arr){
     for(var i = 0; i < arr.length; i++){
-        console.log(arr)
-        var todoContainer = document.createElement('div');
-        var strikeButton = document.createElement("button");
-        var deleteButton = document.createElement("button");
-        var title = document.createElement("h3");
-        var des = document.createElement("p")
-        // var complete = document.createElement("p");
-        var image = document.createElement("IMG");
-        strikeButton.type = "submit"
-        strikeButton.innerText = "Finished"
-        deleteButton.type = "submit"
-        deleteButton.innerText = "DELETE"
-        deleteButton.addEventListener("click", deleteTodo)
-        todoContainer.id = `${arr[i]._id}`
+                console.log(arr)
+                var todoContainer = document.createElement('div');
+                var strikeButton = document.createElement("button");
+                var deleteButton = document.createElement("button");
+                var title = document.createElement("h3");
+                var des = document.createElement("p")
+                // var complete = document.createElement("p");
+                var image = document.createElement("IMG");
+                strikeButton.type = "submit"
+                strikeButton.innerText = "Finished"
+                strikeButton.addEventListener('click', strikeThrough)
+                deleteButton.type = "submit"
+                deleteButton.innerText = "DELETE"
+                deleteButton.addEventListener("click", deleteTodo)
+                todoContainer.id = `${arr[i]._id}`
+        
+        
+                if(arr[i].completed === true){
+                    // todoContainer.style.textDecoration = "line-through";
+                    todoContainer.classList.add("done");
+                }
+                // else{
+                //     todoContainer.style.textDecoration = "none";
+                // }
+                        
 
-
-        if(arr[i].completed === true){
-            todoContainer.style.textDecoration = "line-through";
-            // arr[i].completed === true;
-        }
-
-        // strikeButton.addEventListener("click", function(e) {
-        //     e.target.parentNode.childNodes[1].style.textDecoration = "line-through";
-        // }, true)
-
-        strikeButton.addEventListener("click", function(e) {
-            if(arr[1].completed === false){
-                arr[1].completed = true
-                // todoContainer[1].style.textDecoration = "line-through";
-                e.target.parentNode.childNodes[1].style.textDecoration = "line-through";
-            }else if (arr[1].completed === true){
-                arr[1].completed = false;
-                // todoContainer[1].style.textDecoration = "none";
-                e.target.parentNode.childNodes[1].style.textDecoration = "none";
+                
+                // strikeButton.addEventListener("click", function() {
+                //     // console.log(arr[1].completed)
+                //     if(event.target["data-completed"] === false){
+                //         updateTodoTrue()
+                //         console.log("Hi jessie, this is FALSE")
+                //     }else if (event.target["data-completed"] === true){
+                //         updateTodoFalse()
+                //         console.log("hi jess, this is TRUE")
+                //     }
+                // });
+                // function updateTodoTrue(){
+                //     // e.preventDefault()
+                //     let updatedObject = {
+                //         completed : true
+                //     } 
+                //     axios.put(`https://api.vschool.io/jessie/todo/${arr[1]._id}`, updatedObject).then(function(response){
+                //     console.log(response.data)
+                //     totallyDisplayDataDudeBro([response.data])
+                //     document
+                //     }).catch(function(error){
+                //         console.log(error)
+                //     })
+                // };
+                // function updateTodoFalse(){
+                //     // e.preventDefault()
+                //     let updatedObject = {
+                //         completed : false
+                //     } 
+                //     axios.put(`https://api.vschool.io/jessie/todo/${arr[1]._id}`, updatedObject).then(function(response){
+                //     console.log(response.data)
+                //     totallyDisplayDataDudeBro([response.data])
+                //     }).catch(function(error){
+                //         console.log(error)
+                //     })
+                // }; 
+        
+                title.textContent = arr[i].title
+                des.textContent = arr[i].description
+                // complete.textContent = `Completed: ${arr[i].completed}`
+                image.setAttribute("src",`${arr[i].imgUrl}`);
+                
+                document.getElementById("name").prepend(todoContainer)
+                todoContainer.appendChild(strikeButton);
+                todoContainer.appendChild(title);
+                todoContainer.appendChild(des);
+                // todoContainer.appendChild(complete);
+                todoContainer.appendChild(image);
+                todoContainer.appendChild(deleteButton);
             }
-        });
-
-        title.textContent = arr[i].title
-        des.textContent = arr[i].description
-        // complete.textContent = `Completed: ${arr[i].completed}`
-        image.setAttribute("src",`${response.data[i].imgUrl}`);
-
-        todoContainer.appendChild(strikeButton);
-        todoContainer.appendChild(title);
-        todoContainer.appendChild(des);
-        // todoContainer.appendChild(complete);
-        todoContainer.appendChild(image);
-        todoContainer.appendChild(deleteButton);
-        document.getElementById("name").appendChild(todoContainer)
-    }   
-})
+}
 
 var form = document.getElementById("newTodo");
 form.addEventListener("submit", submitTodo);
@@ -86,21 +107,21 @@ form.addEventListener("submit", submitTodo);
 
 function submitTodo (e){
     e.preventDefault()
-    // var newTitle = form.title.value
-    // var newDescription = form.description.value
-    // var newImage = form.imgUrl.value
     let userObject = {
         title: form.title.value,
         description: form.description.value,
-        completed: false,
         imgUrl: form.imgUrl.value
     };
     axios.post("https://api.vschool.io/jessie/todo",userObject).then(function(response){
         console.log(response.data)
+        totallyDisplayDataDudeBro([response.data])
     }).catch(function(error){
         console.log(error)
-    })            
-}
+    })
+    form.title.value = ""
+    form.description.value = ""
+    form.imgUrl.value = ""            
+};
 // axios.post("https://api.vschool.io/jessie/todo",userObject).then(function(response){
 //     console.log(response.data)
 // }).catch(function(error){
@@ -108,13 +129,31 @@ function submitTodo (e){
 // })            
 // }
 
- 
-//  axios.put(`https://api.vschool.io/jessie/todo/${}`, updatedObject).then(function(response){
+// function updateTodoTrue(){
+//     // e.preventDefault()
+//     let updatedObject = {
+//         completed : true
+//     } 
+//     axios.put(`https://api.vschool.io/jessie/todo/${arr[1]._id}`, updatedObject).then(function(response){
 //     console.log(response.data)
-//  }).catch(function(error){
-//     console.log(error)
-//  })
- 
+//     totallyDisplayDataDudeBro([response.data])
+//     }).catch(function(error){
+//         console.log(error)
+//     })
+// }; 
+// function updateTodoFalse(){
+//     e.preventDefault()
+//     let updatedObject = {
+//         completed : false
+//     } 
+//     axios.put(`https://api.vschool.io/jessie/todo/${arr[1]._id}`, updatedObject).then(function(response){
+//     console.log(response.data)
+//     totallyDisplayDataDudeBro([response.data])
+//     }).catch(function(error){
+//         console.log(error)
+//     })
+// }; 
+
 function deleteTodo(e){
     axios.delete(`https://api.vschool.io/jessie/todo/${e.target.parentElement.id}`).then(function(response){
         console.log(response.data)
@@ -123,7 +162,39 @@ function deleteTodo(e){
         console.log(error)
     })
 
+};
+
+function strikeThrough(e){
+    e.target.parentElement.classList.toggle("done");
+    console.log(e);
+    if(e.target.parentElement.classList.value === "done"){
+        axios.put(`https://api.vschool.io/jessie/todo/${e.target.parentElement.id}`, {completed : true}).then(response =>{
+            console.log(response.data)
+        });
+    }else {
+        axios.put(`https://api.vschool.io/jessie/todo/${e.target.parentElement.id}`, {completed : false}).then(response =>{
+            console.log(response.data)
+        });
+    }
+
 }
 
 
 // document.getElementById("mylist").innerHTML=response.data[i].title;  
+
+//
+//working strikethru button
+// strikeButton.addEventListener("click", function(e) {
+//     if(arr[1].completed === false){
+//         arr[1].completed = true
+//         e.target.parentNode.childNodes[1].style.textDecoration = "line-through";
+//     }else if (arr[1].completed === true){
+//         arr[1].completed = false;
+//         e.target.parentNode.childNodes[1].style.textDecoration = "none";
+//     }
+// });
+
+
+
+/// map over my array of todos and replace the old todo (has same id as new one).
+/// check if the current todo has the same id as the old one.  map and filter (filter returns a new array with less items)
